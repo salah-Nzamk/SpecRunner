5b7b547fc6bcea1b92a11606f948c327d4c47d72
salah yousef <simyousef@gmail.com>
2018-06-08 15:08:19 +0200

    jasmine

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,5 +1,6 @@
 function AddressBook() {
-	this.contacts = []; // add a new property to the class
+    this.contacts = []; // add a new property to the class
+    this.initialComplete = false;
 }
 
 //write a new function for AddresBook to add the Contact
@@ -8,4 +9,19 @@ AddressBook.prototype.addContact = function(contact) {
 }
 AddressBook.prototype.getContact = function(contactIndex) {
 	return this.contacts[contactIndex];
+}
+
+AddressBook.prototype.deleteContact = function(contactIndex) {
+    this.contacts.splice(contactIndex, 1);
+}
+
+AddressBook.prototype.getInitialContacts = function (cb) {
+    var self = this;
+
+    setTimeout(() => {
+        self.initialComplete = true;
+        if (cb) {
+            return cb;
+        }
+    }, 30);
 }
