075fd9b49bd0a8be37e8ea15896f175dd04c98a6
salah yousef <simyousef@gmail.com>
2018-06-08 11:23:51 +0200

    fix bug

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,4 +1,8 @@
 function AddressBook() {
+	this.contacts = []; // add a new property to the class
+}
 
-    
+//write a new function for AddresBook to add the Contact
+AdressBook.prototype.addContact = function(contact) {
+	this.contacts.push(contact);
 }
